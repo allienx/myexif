766ec32a9321291081cb090529a75704c476aad7
Alex Lin <allienx@hashmail.cc>
2020-01-26 22:07:38 -0800

    use consistent live photo filenames

--- src/renameLivePhotos.js
+++ src/renameLivePhotos.js
@@ -2,7 +2,6 @@
 
 const path = require('path')
 
-const first = require('lodash/first')
 const { DateTime } = require('luxon')
 
 const { mkdir, rename } = require('./util/promises/fs')
@@ -13,7 +12,7 @@ const args = process.argv.map(arg => arg)
 args.shift()
 args.shift()
 
-const dir = first(args) || '.'
+const dir = args[0] || '.'
 
 main()
   .catch(err => {
@@ -29,32 +28,51 @@ async function main() {
 
   if (livePhotos.length > 0) {
     await mkdir(path.join(dir, 'live-photos'), { recursive: true })
+    await mkdir(path.join(dir, 'live-photos', 'missing'), { recursive: true })
   }
 
   for (let i = 0; i < livePhotos.length; i++) {
     const livePhoto = livePhotos[i]
 
-    if (!livePhoto.photo || !livePhoto.video) {
-      console.log('Incomplete live photo...', livePhoto)
+    // Move any incomplete photos to the missing folder.
+    if (!livePhoto.video) {
+      await rename(
+        path.join(dir, livePhoto.photo),
+        path.join(dir, 'live-photos', 'missing', livePhoto.photo),
+      )
+
       continue
     }
+
+    // Move the incomplete videos to the missing folder.
+    if (!livePhoto.photo) {
+      await rename(
+        path.join(dir, livePhoto.video),
+        path.join(dir, 'live-photos', 'missing', livePhoto.video),
+      )
+
+      continue
+    }
+
+    const { name: photoName, ext: photoExt } = path.parse(livePhoto.photo)
+    const { ext: videoExt } = path.parse(livePhoto.video)
 
     const dateTimeOriginal = await getExiftoolValue(
       'DateTimeOriginal',
       dir + '/' + livePhoto.photo,
     )
-
     const dt = DateTime.fromFormat(dateTimeOriginal, 'yyyy:MM:dd HH:mm:ss')
     const fmt = dt.toFormat('yyyy-MM-dd_HH-mm-ss')
 
+    // Give the live photo files the same name with their respective extensions.
     await rename(
       path.join(dir, livePhoto.photo),
-      path.join(dir, 'live-photos', `${fmt}_${livePhoto.photo}`),
+      path.join(dir, 'live-photos', `${fmt}_${photoName}${photoExt}`),
     )
 
     await rename(
       path.join(dir, livePhoto.video),
-      path.join(dir, 'live-photos', `${fmt}_${livePhoto.video}`),
+      path.join(dir, 'live-photos', `${fmt}_${photoName}${videoExt}`),
     )
   }
 }
